e17fe6a1e60a0eaab923b2256dc1241222aad927
MD.Tanvir Rahman <tanviredu2018@gmail.com>
2024-08-11 11:24:11 +0600

    Update views.py

--- xml_to_excel/converter/views.py
+++ xml_to_excel/converter/views.py
@@ -5,7 +5,6 @@ import pandas as pd
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import UploadFileForm
-
 def get_text(element):
     return element.text if element is not None else None
 
@@ -44,6 +43,13 @@ def process_xml(file):
     for bol_segment in root.findall('Bol_segment'):
         bol_dict = flatten_dict(xml_to_dict(bol_segment))
         
+        # Extract Trader_segment and Shipping_agent_code within bol_segment
+        trader_segment = bol_segment.find('Trader_segment')
+        if trader_segment:
+            shipping_agent_code = trader_segment.find('Shipping_agent_code')
+            if shipping_agent_code is not None:
+                bol_dict['Shipping_agent_code'] = get_text(shipping_agent_code)
+        
         # Extract ctn segments within bol_segment
         ctn_segments = bol_segment.findall('ctn_segment')
         if ctn_segments:
@@ -55,16 +61,47 @@ def process_xml(file):
             bol_segment_data.append(bol_dict)
 
     df_general = pd.DataFrame(general_segment_data)
-    df_bol = pd.DataFrame(bol_segment_data)
+    data = pd.DataFrame(bol_segment_data)
+    
+    df = pd.DataFrame()
+    df['BL Nbr']       = data['Bol_id_Bol_reference']
+    df['BL Type']      = data['Bol_id_Bol_type_code']
+    df['MBL']          = data['Bol_id_Master_bol_ref_number']
+    df['POL']          = data['Load_unload_place_Port_of_origin_code']
+    df['Carrier Code'] = data['Traders_segment_Carrier_Carrier_code']
+    df['Carrier Name'] = data['Traders_segment_Carrier_Carrier_name']
+    df['Carrier Address'] = data['Traders_segment_Carrier_Carrier_address']
+    df['Exporter Name'] = data['Traders_segment_Exporter_Exporter_name']
+    df['Exporter address'] = data['Traders_segment_Exporter_Exporter_address']
+    df['Notify name'] = data['Traders_segment_Notify_Notify_name']
+    df['Notify address'] = data['Traders_segment_Notify_Notify_address']
+    df['Consignee name'] = data['Traders_segment_Consignee_Consignee_name']
+    df['Consignee address'] = data['Traders_segment_Consignee_Consignee_address']
+    df['Unit Nbr'] = data['ctn_segment_Ctn_reference']
+    df['No.of Pkg'] = data['ctn_segment_Number_of_packages']
+    df['Type Length'] = data['ctn_segment_Type_of_container']
+    df['Frght Kind'] = data['ctn_segment_Status']
+    df['Seal Nbr1'] = data['ctn_segment_Seal_number']
+    df['Cargo Wt (kg)'] = data['ctn_segment_Gross_weight']
+    df['Pkg Code'] = data['Goods_segment_Package_type_code']
+    df['Commodity Original'] = data['Goods_segment_Goods_description']
+    df['CBM'] = data['Goods_segment_Volume_in_cubic_meters']
+    df['Vatable'] = data['Goods_segment_Remarks']
+    df['Shipping Agent'] = data['Traders_segment_Shipping_Agent_Shipping_Agent_code']
+    df['Shipping Agent Name'] = data['Traders_segment_Shipping_Agent_Shipping_Agent_name']
+    df['Cmdty Code'] = data['ctn_segment_Commodity_code']
+    df['IMDG'] = data['Bol_id_DG_status']
+    df['IMDG Code'] = data['ctn_segment_IMCO']
+    df['UN No'] = data['ctn_segment_UN']
 
     with tempfile.NamedTemporaryFile(delete=False, suffix='.xlsx') as tmp:
         with pd.ExcelWriter(tmp.name) as writer:
             df_general.to_excel(writer, sheet_name='General Segment', index=False)
-            df_bol.to_excel(writer, sheet_name='Bol Segment with Ctn', index=False)
+            df.to_excel(writer, sheet_name='Bol Segment with Ctn', index=False)
         tmp_path = tmp.name
 
     return tmp_path
-
+    
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
